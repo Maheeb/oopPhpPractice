2408eb6f6e5f05f7f963c88090bb01d26ab1f35b
Maheeb <maheeb009@gmail.com>
2020-04-24 22:47:02 +0600

    Trait

--- simpleClass.php
+++ simpleClass.php
@@ -8,22 +8,23 @@
 
 class simpleClass{
 
-    public $name ="";
-    private $roll ="";
-
-    function __construct($name,$roll)
-    {
-        $this->name = $name;
-        $this->roll = $roll;
+    public function sayHello(){
+        echo "I am parent class <br>";
     }
 }
 
-$obj_1 = new simpleClass("Maheeb",'cse05807028');
-$obj_2 = clone $obj_1;
-$obj_2->name = "Azmaeen";
+trait test{
+    public function testTrait(){
+        echo "\n I am form trait class";
+    }
+}
 
-echo $obj_1->name ."<br>";
-echo $obj_2->name;
+class child extends simpleClass{
+    use test;
 
+}
 
+$obj = new child();
+$obj->sayHello();
+$obj->testTrait();
 
